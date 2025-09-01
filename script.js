ea709b72187873d272bb71afade840ef828a13ee
dawc17 <dawid.czaplicki2008@gmail.com>
2025-09-01 19:08:04 +0200

    init

---
+++ script.js
@@ -0,0 +1,73 @@
+let handPose;
+let faceMesh;
+
+let video;
+
+let faceOptions = { maxFaces: 1, refineLandmarks: false, flipped: true };
+let faces = [];
+let connectionsFace;
+
+let hands = [];
+let connectionsHand;
+
+function preload(){
+    handPose = ml5.handPose({flipped: true});
+    faceMesh = ml5.faceMesh(faceOptions);
+}
+
+function setup() {
+    createCanvas(680, 460);
+    video = createCapture(VIDEO, {flipped: true});
+    video.size(width, height);
+    video.hide();
+    connectionsHand = handPose.getConnections();
+
+    handPose.detectStart(video, gotHands);
+    faceMesh.detectStart(video, gotFaces);
+}
+
+function gotFaces(results) {
+    faces = results;
+}
+
+function gotHands(results) {
+    hands = results;
+}
+
+function draw() {
+    background(0);
+    //image(video, 0, 0, width, height);
+    for (let i = 0; i < faces.length; i++) {
+        let face = faces[i];
+        for (let j = 0; j < face.keypoints.length; j++) {
+            let keypoint = face.keypoints[j];
+            fill(0, 255, 0);
+            noStroke();
+            circle(keypoint.x, keypoint.y, 5);
+        }
+    }
+
+    for (let i = 0; i < hands.length; i++) {
+        let hand = hands[i];
+        // Draw skeletal connections first so keypoint circles render on top
+        if (connectionsHand && connectionsHand.length && hand.keypoints && hand.keypoints.length) {
+            stroke(0, 255, 0);
+            strokeWeight(2);
+            noFill();
+            for (let c = 0; c < connectionsHand.length; c++) {
+                const [a, b] = connectionsHand[c];
+                const kpA = hand.keypoints[a];
+                const kpB = hand.keypoints[b];
+                if (kpA && kpB) {
+                    line(kpA.x, kpA.y, kpB.x, kpB.y);
+                }
+            }
+        }
+        for (let j = 0; j < hand.keypoints.length; j++) {
+            let keypoint = hand.keypoints[j];
+            fill(0, 255, 0);
+            noStroke();
+            circle(keypoint.x, keypoint.y, 10);
+        }
+    }
+}
